0e1137c6240b2202d820597d6dd4f545c6e892c7
Alan Fidelino <55466923+afimonsido@users.noreply.github.com>
2021-10-24 16:53:05 +1100

    Updates

---
+++ static/js/main.6b53fcb4.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpauth0=this.webpackJsonpauth0||[]).push([[0],{11:function(t,n,e){"use strict";e.r(n);e(1);var c=e(3),r=e.n(c),i=(e(8),e(9),e(0));var s=function(){return Object(i.jsx)("div",{children:Object(i.jsx)("h2",{children:"HELLO TESTING..."})})};r.a.render(Object(i.jsx)(i.Fragment,{children:Object(i.jsx)(s,{})}),document.getElementById("root"))},8:function(t,n,e){},9:function(t,n,e){}},[[11,1,2]]]);
+//# sourceMappingURL=main.6b53fcb4.chunk.js.map
